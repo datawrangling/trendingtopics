bbb875dc1dc4f822a2a74109fc1a98ea98e2ee99
Pete Skomoroch <pete@datawrangling.com>
2009-06-08 16:08:58 -0400

    only cache autocomplete page... add search button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,6 @@ class PagesController < ApplicationController
   layout 'pages'#, :except => [:auto_complete_for_search_query]
   use_google_charts
 
-  caches_page :index
-  caches_page :show
   caches_page :auto_complete_for_search_query
 
   def auto_complete_for_search_query
