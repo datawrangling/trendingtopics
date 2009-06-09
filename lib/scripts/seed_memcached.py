292ea9ea5fff8c94e68be5056d2182e92fd96202
Pete Skomoroch <pete@datawrangling.com>
2009-06-09 16:35:37 -0400

    added memcache prefetch task

---
+++ lib/scripts/seed_memcached.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+seed_memcached.py
+
+quick hack to prepopulate memcached with inital letters for autocomplete
+
+Created by Peter Skomoroch on 2009-06-09.
+Copyright (c) 2009 Data Wrangling LLC. All rights reserved.
+"""
+
+import sys
+import os
+
+
+
+SITE = 'http://www.trendingtopics.org/'
+
+def fetchpages():
+  # TODO: read queries and site from a file...
+  autocomplete_template = 'pages/auto_complete_for_search_query?search%5Bquery%5D'
+
+  prefetch = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  urls = [SITE + autocomplete_template + x for x in prefetch]  
+  digraph = "th he an in er on re ed nd ha at en es of nt ea ti to io le is ou ar as de rt ve"
+  digraph_urls = [SITE + autocomplete_template + x for x in digraph.split()]
+  urls.extend(digraph_urls)
+
+  os.system('curl %s' % SITE)
+  for url in urls:
+    print 'curl %s' % (url)
+    os.system('curl %s' % (url))
+
+
+def main():
+	fetchpages()
+
+
+if __name__ == '__main__':
+	main()
+
