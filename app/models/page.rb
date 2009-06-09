c1cb7c75e6cc10620fd0d22785a8fddfca0c6707
Pete Skomoroch <pete@datawrangling.com>
2009-06-09 03:44:51 -0400

    now with pretty urls: http://localhost:3000/pages/Britain%27s_Got_Talent

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,9 @@ class Page < ActiveRecord::Base
   named_scope :title_like, lambda { |query| { :conditions => ['title like ?', "#{query}%"], :order => '`monthly_trend` DESC', :limit => 12 } }
   named_scope :full_title_like, lambda { |query| { :conditions => ['title like ?', "%#{query}%"], :order => '`monthly_trend` DESC', :limit => 12 } }  
   
+  def to_param
+    "#{url}"
+  end
   
   
   
