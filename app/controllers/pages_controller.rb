cc5cf08613b8440a98cc8195dfcfa41df4e6097e
Pete Skomoroch <pete@datawrangling.com>
2009-06-09 02:18:11 -0400

    switched back to old trend search approach

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
     
   def index
     if params[:search]
-      @pages = Page.full_title_like(params["search"]["query"]).paginate :page => params[:page], :order => 'total_pageviews DESC', :per_page => APP_CONFIG['articles_per_page']  
+      @pages = Page.title_like(params["search"]["query"]).paginate :page => params[:page], :order => 'monthly_trend DESC', :per_page => APP_CONFIG['articles_per_page']  
       
     else   
       @pages = Page.paginate :page => params[:page], :order => 'monthly_trend DESC', :per_page => APP_CONFIG['articles_per_page']  
