fdab15ed6cd093671b05fea4edfbc8a897a27c48
Pete Skomoroch <pete@datawrangling.com>
2009-06-15 10:56:00 -0400

    date is a reserved word

--- lib/hive/hive_daily_merge.sql
+++ lib/hive/hive_daily_merge.sql
@@ -1,7 +1,7 @@
 -- 1. creates staging tables
-CREATE TABLE raw_daily_pagecounts_table (redirect_title STRING, date STRING, pageviews STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
+CREATE TABLE raw_daily_pagecounts_table (redirect_title STRING, dates STRING, pageviews STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
 
-CREATE TABLE daily_pagecounts_table (page_id BIGINT, date STRING, pageviews STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
+CREATE TABLE daily_pagecounts_table (page_id BIGINT, dates STRING, pageviews STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
 
 CREATE TABLE redirect_table (redirect_title STRING, true_title STRING, page_id BIGINT, page_latest BIGINT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
 
@@ -28,7 +28,7 @@ LOAD DATA LOCAL INPATH '/mnt/daily_timelines.txt' OVERWRITE INTO TABLE daily_tim
 -- 4. normalizes python streaming output table "raw_daily_pagecounts" with page_id
 
 INSERT OVERWRITE TABLE daily_pagecounts_table
-SELECT redirect_table.page_id, raw_daily_pagecounts_table.date, raw_daily_pagecounts_table.pageviews, raw_daily_stats_table.total_pageviews FROM redirect_table JOIN raw_daily_pagecounts_table ON (redirect_table.redirect_title = raw_daily_pagecounts_table.redirect_title);
+SELECT redirect_table.page_id, raw_daily_pagecounts_table.dates, raw_daily_pagecounts_table.pageviews, raw_daily_stats_table.total_pageviews FROM redirect_table JOIN raw_daily_pagecounts_table ON (redirect_table.redirect_title = raw_daily_pagecounts_table.redirect_title);
 
 -- 5. populate new_daily_timelines: merges old daily_timelines with new, inserts into "new_daily_timelines"
 
