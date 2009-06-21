655f1fdfc4af18a4dc8e1259afba7685a99552c9
Pete Skomoroch <pete@datawrangling.com>
2009-06-21 00:43:36 -0400

    add feature index, swap idenitfier in daily trends query

--- lib/sql/load_featured_pages.sql
+++ lib/sql/load_featured_pages.sql
@@ -1,7 +1,12 @@
 TRUNCATE TABLE new_featured_pages;
+ALTER TABLE new_featured_pages DISABLE KEYS;
 
 LOAD DATA LOCAL INFILE '/mnt/featured_pages.txt'
 INTO TABLE new_featured_pages
 FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
 (page_id);
+
+create index featured_index on new_featured_pages (page_id);
+
+ALTER TABLE new_featured_pages ENABLE KEYS;
