e58abf8f178a48db63126a12f523ce7f98a9c5ae
Pete Skomoroch <pete@datawrangling.com>
2009-06-21 02:27:33 -0400

    changed featured update statement

--- lib/sql/load_featured_pages.sql
+++ lib/sql/load_featured_pages.sql
@@ -1,6 +1,8 @@
 TRUNCATE TABLE new_featured_pages;
 ALTER TABLE new_featured_pages DISABLE KEYS;
 
+CALL dropindex('new_featured_pages', 'featured_index');
+
 LOAD DATA LOCAL INFILE '/mnt/featured_pages.txt'
 INTO TABLE new_featured_pages
 FIELDS TERMINATED BY '\t'
