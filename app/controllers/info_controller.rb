612c7698478383db5f2b2a6cf590cc04e91a5321
Pete Skomoroch <pete@datawrangling.com>
2009-06-21 17:33:52 -0400

    finance cleanup

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -10,7 +10,24 @@ class InfoController < ApplicationController
   end
   
   def finance
-    @pages = Page.paginate(:page => params[:page], :joins => :company, :order => 'monthly_trend DESC', :per_page => APP_CONFIG['articles_per_page'])     
+    @pages = Page.paginate(:page => params[:page], :joins => :company, :order => 'monthly_trend DESC', :per_page => APP_CONFIG['articles_per_page'])   
+    
+    @page = Page.find(:first,  :joins => :company, :order => 'monthly_trend DESC' )
+    if @page.nil?
+      @page = Page.find(:first)
+    end
+          
+    
+    unless params[:page]
+      params[:page]='1'
+    end  
+    
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @pages }
+    end    
+      
   end  
   
   def alphabet
