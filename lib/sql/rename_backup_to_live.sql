c7025b5ce3ff91d69a0ca31d792f5ce37de8d66e
Pete Skomoroch <pete@datawrangling.com>
2009-06-22 23:38:08 -0400

    backup sql swap script

---
+++ lib/sql/rename_backup_to_live.sql
@@ -0,0 +1,4 @@
+RENAME TABLE pages TO new_pages, backup_pages TO pages;
+RENAME TABLE daily_timelines TO new_daily_timelines, backup_daily_timelines TO daily_timelines;
+RENAME TABLE daily_trends TO new_daily_trends, backup_daily_trends TO daily_trends;
+RENAME TABLE featured_pages TO new_featured_pages, backup_featured_pages TO featured_pages;
