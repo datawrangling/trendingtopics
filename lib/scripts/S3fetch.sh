2b11f1a3435f83b287b9d8abe054d8d169db3466
Pete Skomoroch <pete@datawrangling.com>
2009-07-08 05:48:19 -0400

    handle duplicate map attempts

--- lib/scripts/S3fetch.sh
+++ lib/scripts/S3fetch.sh
@@ -2,6 +2,8 @@
 
 # Quick hack to grab directory from S3 to local target directory using s3cmd
 #
+# assumes format is s3://trendingtopics/archive/20090702/daily_timelines/attempt_200907072330_0011_m_000068_1
+#
 # example usage:
 # bash /mnt/app/current/lib/scripts/S3fetch.sh s3://trendingtopics/archive/20090628/pages/ /mnt/pages
 # TODO Replace this script and timeout logic for s3cmd with custom s3 downloader  
@@ -17,12 +19,13 @@ for filename in $PAGES
 do
   echo "downloading $filename"
   TARGET=`echo $filename | cut -d'/' -f7`
+  PARTNAME=`echo $filename | cut -d'/' -f7 | cut -d'_' -f1-5`
   
   RETVAL=1
   while [ $RETVAL -ne 0 ]
   do
     # wait 20 seconds for each download to complete, else abort & retry
-    bash /mnt/app/current/lib/scripts/cmdtimeout -t 60 s3cmd --force get $filename  $DESTINATION/part-$COUNTER 
+    bash /mnt/app/current/lib/scripts/cmdtimeout -t 60 s3cmd --force get $filename  $DESTINATION/$PARTNAME 
     RETVAL=$?
     if [ $RETVAL -eq 1  ]; then
       echo download attempt failed, pausing
